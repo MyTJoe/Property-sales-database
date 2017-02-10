c963aaeba7d81acd2e77e6514740d3c5c6a66cc4
Joe <josephelangley@gmail.com>
2017-02-10 11:01:20 -0500

    just some sample data for front end to use

---
+++ src/main/java/com/theironyard/controllers/PropertyController.java
@@ -0,0 +1,45 @@
+package com.theironyard.controllers;
+
+import com.theironyard.entities.Address;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+import javax.annotation.PostConstruct;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+
+@Controller
+public class PropertyController {
+    private static List<Address> addresses = new ArrayList<>();
+
+    @PostConstruct
+    public void init() throws FileNotFoundException {
+
+        if (addresses.size() == 0) {
+            File f = new File("addresses.csv");
+            Scanner fileScanner = new Scanner(f);
+
+            while (fileScanner.hasNext()) {
+
+                String line = fileScanner.nextLine();
+                String[] columns = line.split(",");
+                Address address = new Address(columns[0], Integer.valueOf(columns[1]), columns[2]);
+
+                addresses.add(address);
+            }
+        }
+    }
+    @ResponseBody
+    @RequestMapping(path = "/address.json", method = RequestMethod.GET)
+    public List<Address> jsonHome() {
+        return addresses;
+    }
+}
