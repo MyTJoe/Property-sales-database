3bbf1eba3a021b6cca3ece2e1f2cbe653827205c
Joe <josephelangley@gmail.com>
2017-02-18 21:23:42 -0500

    fixing a bug or 2

--- src/main/java/com/theironyard/repositories/TaxRepository.java
+++
@@ -1,8 +0,0 @@
-package com.theironyard.repositories;
-
-import com.theironyard.entities.TaxRecords;
-import org.springframework.data.repository.CrudRepository;
-
-public interface TaxRepository extends CrudRepository<TaxRecords, Integer> {
-    TaxRecords findByCategory(String Category);
-}
