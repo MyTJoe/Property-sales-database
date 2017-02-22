c07ee84373ff4acedfcaf4c7be150bc946fd3540
KingPra <kingi.pra@gmail.com>
2017-02-22 18:09:50 -0500

    added pagination button limits

--- js/controllers/listings.js
+++ js/controllers/listings.js
@@ -2,6 +2,7 @@ module.exports = {
     name: 'ListingsController',
     func: function ($scope, $state, ListingsService, MapService) {
         // county select box
+        let count = 0;
         $scope.displayCounty = 'Harnett County';
         let pickCounty = 'harnett';
         $scope.countyList = [
@@ -22,15 +23,13 @@ module.exports = {
                 label: 'Rutherford County'
             }
         ];
-
         //pagination maybe
-        $scope.btnNums =  (loc) => {
-            let count = 0;
-            console.log(`work already ${loc}`);
+        $scope.btnNums = (loc) => {
+            //let count = 0;
+            let max = 0;
             for (let i = 0; i < loc.length / 10; i++) {
                 count++;
                 btnCount.push(count);
-                console.log(`some num here: ${btnCount}`);
             }
                 return btnCount;
         };
@@ -41,19 +40,23 @@ module.exports = {
          let endNum = 10;
          $scope.num = btnCount;
         $scope.showPage = (operator) => {
-            if (operator === 'next') {
+            if (operator === 'next' && currentPage < count) {
                 startNum += 10;
                 endNum += 10;
-            } else if (operator === 'back') {
+                currentPage++;
+            } else if (operator === 'back' && startNum > 1) {
                 startNum = startNum - 10;
                 endNum =  endNum - 10;
-            } else {            
+                currentPage --;
+                console.log('back back fourth and fourth')
+            } else if (Number.isInteger(operator) === true ) {
+                console.log('numbers for all');            
              startNum = (operator - 1) * 10;
              endNum = operator * 10;
+             currentPage = operator;
             }
             // A bit wasteful because it redoes a bit AJAX request for each page load.
             ListingsService.getLoc(pickCounty).then(function (listings) {
-                //$scope.locations = listings;
                 $scope.locations = listings.slice(startNum, endNum);
             });
         };
@@ -64,7 +67,6 @@ module.exports = {
             $scope.displayCounty = item.label;
             pickCounty = item.value;
             ListingsService.getLoc(pickCounty).then(function (listings) {
-                //$scope.locations = listings;
             });
         };
         //getting array of locations 
@@ -73,7 +75,6 @@ module.exports = {
             ListingsService.getLoc(initCounty).then(function (listings) {
                 let allListings = listings
                 $scope.locations = allListings.slice(startNum, endNum);
-                console.log(`this is all list: ${allListings}`)
                 $scope.btnNums(listings);
             });
         };
